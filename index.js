040b192f1bd366c99c26c6b9177592ba6a921c17
Alexander Royal <alexander.de.royal@gmail.com>
2023-06-29 19:33:47 -0500

    routing exercises completed

---
+++ index.js
@@ -0,0 +1,95 @@
+// const fs = require('fs');
+// const http = require('http');
+// /////////////////////////////////////
+// // Files
+//
+// // Blocking, synchronous way
+// // const textIn = fs.readFileSync('./input.txt', 'utf-8');
+// // console.log(textIn);
+// // const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// // fs.writeFileSync('./output.txt', textOut);
+// // console.log("File Written");
+//
+// // Non-blocking, asynchronous way
+// // fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+// //     if (err) return console.log('ERROR')
+// //     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+// //         console.log(data2);
+// //         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+// //             console.log(data3);
+// //
+// //             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err =>{
+// //                 console.log('Your file has been written');
+// //             })
+// //         });
+// //     });
+// // });
+// // console.log("Will read file!")
+//
+// //////////////////////////////////
+// // Server
+//
+// const server = http.createServer((req,res) => {
+//     res.end('Hello from the server')
+// });
+//
+// server.listen(8000, '127.0.0.1', () => {
+//     console.log("Server running on port 8000");
+// });
+
+// ------------------Redo--------------------
+// const fs = require('fs')
+// const http = require('http');
+
+//////////////////////////// FILE
+
+// const fs = require('fs')
+// BLOCKING SYNCHRONOUS WAY
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const hello = "Hello World";
+// console.log(hello);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written');
+
+// NON-BLOCKING NON-SYNCHRONOUS WAY
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+//
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', err => {
+//                 console.log('Your file has been written')
+//             })
+//         });
+//     });
+// });
+// console.log('Will read file!')
+
+////////////////////// SERVER
+
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req,res) => {
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the overview');
+    } else if (pathName === '/product'){
+        res.end('This is the product')
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        });
+        res.end("<h1>Fage not pound</h1>")
+    }
+    res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log("Server started on port 8000")
+})
