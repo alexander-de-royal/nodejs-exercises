94ea529f469e48f567fa2582e2eec204c9025fad
Alexander Royal <alexander.de.royal@gmail.com>
2023-07-01 12:38:33 -0500

    added html template but it did not work

--- index.js
+++ index.js
@@ -68,22 +68,43 @@
 // });
 // console.log('Will read file!')
 
-////////////////////// SERVER
+
 
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+////////////////////// SERVER
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/templates-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/templates-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/templates-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req,res) => {
     const pathName = req.url;
 
+        // Overview Page
     if(pathName === '/' || pathName === '/overview'){
-        res.end('This is the overview');
+        res.writeHead(200, {'Content-type': 'text/html'})
+        res.end(tempOverview);
+
+        // Product Page
     } else if (pathName === '/product'){
-        res.end('This is the product')
+        res.end('This is the product');
+
+        // API
+    }else if (pathName === '/api'){
+        res.writeHead(200, {'Content-type': 'application/json'})
+        res.end(data);
+
+        // Not Found
     } else {
         res.writeHead(404, {
-            'Content-type': 'text/html'
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
         });
         res.end("<h1>Fage not pound</h1>")
     }
