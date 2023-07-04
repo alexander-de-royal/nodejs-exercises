55103c9e5fd4024a03ee3213e01841d919bc11ca
Alexander Royal <alexander.de.royal@gmail.com>
2023-07-04 08:23:02 -0500

    javascript refresher started

---
+++ play.js
@@ -0,0 +1,8 @@
+var name = 'Max';
+var age = 29;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby){
+    return `name is ${userName}, age is ${userAge}, has hobbies ${userHasHobby}`
+}
+console.log(name, age, hasHobbies);
