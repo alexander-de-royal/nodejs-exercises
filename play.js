9e38ba6e3dfa4aba1d9f7375f49f4d036b2ed577
Alexander Royal <alexander.de.royal@gmail.com>
2023-07-04 11:49:28 -0500

    arrow function completed

--- play.js
+++ play.js
@@ -2,7 +2,23 @@ var name = 'Max';
 var age = 29;
 var hasHobbies = true;
 
-function summarizeUser(userName, userAge, userHasHobby){
+age = 30;
+
+// const summarizeUser = function summarizeUser(userName, userAge, userHasHobby){
+//     return `name is ${userName}, age is ${userAge}, has hobbies ${userHasHobby}`
+// }
+// console.log(name, age, hasHobbies);
+
+const summarizeUser = (userName, userAge, userHasHobby) => {
+
     return `name is ${userName}, age is ${userAge}, has hobbies ${userHasHobby}`
 }
 console.log(name, age, hasHobbies);
+
+const add = (a,b) => {
+    return a + b;
+}
+console.log(add(1,2));
+
+console.log(summarizeUser(name, age, hasHobbies));
+
