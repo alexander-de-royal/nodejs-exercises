2d8f0979101917640e94c6f9dee254abf1b390ab
Alexander Royal <alexander.de.royal@gmail.com>
2023-09-09 17:49:30 +0545

    node import export completed

---
+++ math.js
@@ -0,0 +1,27 @@
+// module.exports = (a,b) => {
+//     return a + b;
+// };
+
+// module.exports = add;
+//
+// module.exports.add = (a,b) => {
+//     return a + b;
+// };
+//
+// module.exports.subtract = (a,b) => {
+//     return a - b;
+// }
+
+// module.exports = {
+//     add,
+//     subtract
+// }
+
+
+exports.add = (a,b) => {
+    return a + b;
+};
+
+exports.subtract = (a,b) => {
+    return a - b;
+}
